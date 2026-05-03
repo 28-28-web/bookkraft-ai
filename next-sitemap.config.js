175204596650310282166fc86044f7427fa63ccf
28-28-web <januinetech7979@gmail.com>
2026-05-03 21:43:10 +0600

    your message

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,17 @@ module.exports = {
   siteUrl: 'https://bookkraftai.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  transform: async (config, path) => {
+    try {
+      await fetch(
+        `https://api.indexnow.org/indexnow?url=${config.siteUrl}${path}&key=462dc3ce6a474e9fac2a2423c20fe204`
+      )
+    } catch (e) {}
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }
+  },
 }
