5ccce1a523905af2ad0e24d3a359aa8320ca0be6
28-28-web <januinetech7979@gmail.com>
2026-05-07 20:45:29 +0600

    feat: add Microsoft Clarity analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -71,7 +71,13 @@ export default function RootLayout({ children }) {
               page_path: window.location.pathname
             });
           `}
-        </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wfa9btfpsh");
+</script>
       </head>
 
       <body>
